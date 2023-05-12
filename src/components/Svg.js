66261e22e82d57bb56e636439763b167057ff606
tvancisin <tomasvancisin@gmail.com>
2023-05-12 23:57:05 +0100

    ho

--- src/components/Svg.js
+++ src/components/Svg.js
@@ -1,6 +1,6 @@
 import React, { useState,useRef, useEffect } from 'react'
 import * as d3 from 'd3';
-import { MapContainer, TileLayer, Polygon, Marker, Popup } from 'react-leaflet'
+import { MapContainer, SVGOverlay, TileLayer, Polygon, Marker, Popup } from 'react-leaflet'
 import {statesData} from '../data/UScountries.js';
 
 function Svg() {
@@ -8,14 +8,17 @@ function Svg() {
     const [theData, settheData] = useState ([]);
     const ref = useRef()
 
-
     useEffect(() => {
         d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv", 
         function (theData) {
             return { date : d3.timeParse("%Y-%m-%d")(theData.date), value : theData.value }
         }).then(data => settheData(data))
     }, []);
-    console.log(theData);
+
+    const bounds = [
+    [51.49, -0.08],
+    [51.5, -0.06],
+    ]
 
   return (
     // <svg classVjName='svg-environment'>
@@ -26,13 +29,20 @@ function Svg() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    {statesData.features.map((state) => {
+    <SVGOverlay attributes={{ stroke: 'red' }} bounds={bounds}>
+      <rect x="0" y="0" width="100%" height="100%" fill="blue" />
+      <circle r="5" cx="10" cy="10" fill="red" />
+      <text x="50%" y="50%" stroke="white">
+        text
+      </text>
+    </SVGOverlay>
+    {/* {statesData.features.map((state) => {
         const coordinates = state.geometry.coordinates[0].map((item)=> [item[1], item[0]])
         return (
             <Polygon pathOptions={{fillColor:"black", fillOpacity: 0.5}} positions={coordinates} />
         )
     })
-    }
+    } */}
 
     <Marker position={[51.505, -0.09]}>
         <Popup>
