be712895cf9bf9d58ed5817bf97024a3991d4043
Tomas <tomasvancisin@gmail.com>
2023-06-08 17:16:51 +0100

    gre

--- src/components/Svg.js
+++ src/components/Svg.js
@@ -7,7 +7,7 @@ import * as d3Geo from "d3-geo";
 function Svg({data, otherData, indiaData}) {
 
   const ref = useRef()
-  // function D3Layer() {
+  function D3Layer() {
     const [gata, setGata] = useState ([]);
     const [idata, setIdata] = useState ([]);
     
@@ -29,77 +29,77 @@ function Svg({data, otherData, indiaData}) {
       .domain([0, 275])
       .range([1.5, 100])
 
-    // const map = useMap();
-
-    // useEffect(()=>{
-    //   L.svg().addTo(map);
-    //   d3.select("svg")
-    //     .selectAll("myCircles")
-    //     .data(idata)
-    //     .join("circle")
-    //       .attr("cx", d => map.latLngToLayerPoint([d.lat, d.long]).x)
-    //       .attr("cy", d => map.latLngToLayerPoint([d.lat, d.long]).y)
-    //       .attr("r", 5)
-    //       .style("fill", "yellow")
-    //       // .attr("stroke", "yellow")
-    //       .attr("stroke-width", 3)
-    //       .attr("fill-opacity", .5)
-      
-    //   function update() {
-    //     d3.selectAll("circle")
-    //       .attr("cx", d => map.latLngToLayerPoint([d.lat, d.long]).x)
-    //       .attr("cy", d => map.latLngToLayerPoint([d.lat, d.long]).y)
-    //   }
-    
-    //   map.on("moveend", update)
-    // },[idata])
+    const map = useMap();
 
     useEffect(()=>{
-      console.log("AYAYA");
-      if (idata.length == 0) {
-        return;
+      L.svg().addTo(map);
+      d3.select("svg")
+        .selectAll("myCircles")
+        .data(idata)
+        .join("circle")
+          .attr("cx", d => map.latLngToLayerPoint([d.lat, d.long]).x)
+          .attr("cy", d => map.latLngToLayerPoint([d.lat, d.long]).y)
+          .attr("r", 10)
+          .style("fill", "white")
+          // .attr("stroke", "yellow")
+          .attr("stroke-width", 3)
+          .attr("fill-opacity", 1)
+      
+      function update() {
+        d3.selectAll("circle")
+          .attr("cx", d => map.latLngToLayerPoint([d.lat, d.long]).x)
+          .attr("cy", d => map.latLngToLayerPoint([d.lat, d.long]).y)
       }
-      if (ref.current == null) {
-        return;
-      }
-      idata.sort(function(x, y){
-          return d3.ascending(x.start, y.start);
-      })
+    
+      map.on("moveend", update)
+    },[idata])
 
-      const bullcrap = d3.groups(idata, d => d.start)
+    // useEffect(()=>{
+    //   console.log("AYAYA");
+    //   if (idata.length == 0) {
+    //     return;
+    //   }
+    //   if (ref.current == null) {
+    //     return;
+    //   }
+    //   idata.sort(function(x, y){
+    //       return d3.ascending(x.start, y.start);
+    //   })
 
-      console.log(bullcrap)
+    //   const bullcrap = d3.groups(idata, d => d.start)
 
-      const svgElement = d3.select(ref.current);
-      const x = d3.scaleTime()
-          .domain(d3.extent(bullcrap, function(d) { return d[0]; }))
-          .range([ 0, 460 ]);
+    //   console.log(bullcrap)
 
-      svgElement.append("g")
-          .attr("transform", `translate(0, 400)`)
-          .call(d3.axisBottom(x));
+    //   const svgElement = d3.select(ref.current);
+    //   const x = d3.scaleTime()
+    //       .domain(d3.extent(bullcrap, function(d) { return d[0]; }))
+    //       .range([ 0, 460 ]);
 
-      const y = d3.scaleLinear()
-          .domain([0, d3.max(bullcrap, function(d) { return +d[1].length; })])
-          .range([ 400, 0 ]);
+    //   svgElement.append("g")
+    //       .attr("transform", `translate(0, 400)`)
+    //       .call(d3.axisBottom(x));
 
-      svgElement.append("g")
-          .call(d3.axisLeft(y));
+    //   const y = d3.scaleLinear()
+    //       .domain([0, d3.max(bullcrap, function(d) { return +d[1].length; })])
+    //       .range([ 400, 0 ]);
 
-      svgElement.append("path")
-          .datum(bullcrap)
-          .attr("fill", "none")
-          .attr("stroke", "steelblue")
-          .attr("stroke-width", 1.5)
-          .attr("d", d3.line()
-              .curve(d3.curveMonotoneX)
-              .x(function(d) { return x(d[0]) })
-              .y(function(d) { return y(d[1].length) })
-              )
-    },[idata, ref])
+    //   svgElement.append("g")
+    //       .call(d3.axisLeft(y));
+
+    //   svgElement.append("path")
+    //       .datum(bullcrap)
+    //       .attr("fill", "none")
+    //       .attr("stroke", "steelblue")
+    //       .attr("stroke-width", 1.5)
+    //       .attr("d", d3.line()
+    //           .curve(d3.curveMonotoneX)
+    //           .x(function(d) { return x(d[0]) })
+    //           .y(function(d) { return y(d[1].length) })
+    //           )
+    // },[idata, ref])
 
 
-  // }
+  }
     // const [theData, settheData] = useState ([]);
 
     // useEffect(() => {
@@ -113,21 +113,21 @@ function Svg({data, otherData, indiaData}) {
 
   return (
     <div>
-      <svg
+      {/* <svg
           ref={ref}
-      />
-    {/* <MapContainer center={[47, 2]} zoom={3} scrollWheelZoom={true}>
+      /> */}
+    <MapContainer center={[47, 2]} zoom={3} scrollWheelZoom={true}>
     <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
     />
     <LayerGroup>
           <D3Layer />
-    </LayerGroup> */}
+    </LayerGroup>
     
-    {/* <GeoJSON data={data.features}/> */}
+    <GeoJSON data={data.features}/>
 
-    {/* {data.features.map((state) => {
+    {data.features.map((state) => {
         const coordinates = state.geometry.coordinates.map((item)=> {
           return item[0].length === 2 ? item.map((item)=>[item[1],item[0]]) : item[0].map((item)=>[item[1],item[0]])
           // if (item[0].length === 2){
@@ -139,11 +139,11 @@ function Svg({data, otherData, indiaData}) {
         })
 
         return (
-            <Polygon pathOptions={{fillColor:"none", weight: 0.5, fillOpacity: 0.05}} positions={coordinates} />
+            <Polygon pathOptions={{fillColor:"white", weight: 0.01, fillOpacity:-0.1}} positions={coordinates} />
         )
     })
-    } */}
-    {/* </MapContainer> */}
+    }
+    </MapContainer>
     </div>
 
   )
